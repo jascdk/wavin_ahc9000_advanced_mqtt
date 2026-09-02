c159540d33c8946b81551dde81d9e1ed288bffb6
Jacob Scherrebeck <35434108+jascdk@users.noreply.github.com>
2026-09-03 00:18:41 +0200

    Version 2.4.1

--- src/AppConfig.h
+++ src/AppConfig.h
@@ -2,7 +2,7 @@
 #define APPCONFIG_H
 
 #include <Arduino.h>
-#define FIRMWARE_VERSION "2.3.0"
+#define FIRMWARE_VERSION "2.4.1"
 
 // Board Specific Configuration
 #if defined(CONFIG_IDF_TARGET_ESP32C3) || defined(ESP32C3)
